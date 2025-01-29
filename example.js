c05687dc2b3f23e0e9e0c1f9d0864c7ef074cccc
headwindtrend <128255279+headwindtrend@users.noreply.github.com>
2025-01-29 12:00:59 +0800

    Update example.js
    added a `playback speed` feature which can be specified by adding this `&speed=` optional parameter. for instance, for 1.5 speed playback, add this `&speed=1.5` to the url. and i also took the opportunity to make some minor refinement to the code as well.

--- example.js
+++ example.js
@@ -1,12 +1,17 @@
 javascript: (function() {
 
 const targetVideo = "https://www.youtube.com/watch?v=SUEzksc3CXA";
-const sampleSegmentsSpec = "1647-1700,12:34-13:00";
-const loopFlag = "1";
+const segmentsSpec = "1647-1670,12:34-13:00";
+const loopFlag = false;
+const playbackRate = "1.5";
 
-let videoURL = targetVideo + "&segments=" + sampleSegmentsSpec;
+let videoURL = targetVideo;
+if (typeof segmentsSpec !== "undefined" && segmentsSpec)
+  videoURL += "&segments=" + segmentsSpec;
 if (typeof loopFlag !== "undefined" && loopFlag)
-  videoURL += "&loop=" + loopFlag;
+  videoURL += "&loop=1";
+if (typeof playbackRate !== "undefined" && playbackRate)
+  videoURL += "&speed=" + playbackRate;
 
 var onYouTubeIframeAPIReady = async function() {
   const videoURL = window.top.localStorage.getItem("vspb_thisURL");
@@ -14,8 +19,9 @@ var onYouTubeIframeAPIReady = async function() {
   const ytvid = urlParams.get("v");
   if (ytvid) {
     const segmentsParam = urlParams.get("segments");
+    let segments;
     if (segmentsParam) {
-      const segments = segmentsParam.split(",").map(pair => {
+      segments = segmentsParam.split(",").map(pair => {
         const [start, end] = pair.split("-").map(tstr => {
           if (tstr.includes(":")) {
             if (tstr.split(":").length == 2) {
@@ -31,54 +37,60 @@ var onYouTubeIframeAPIReady = async function() {
         });
         return { start, end };
       });
-      const loop = urlParams.get("loop");
-      await new Promise(resolve => {
-        new YT.Player("player", {
-          videoId: ytvid, width: "100%", height: "100%",
-          events: {
-            "onReady": event => {
-              event.target.playVideo();
-              const d = document.querySelector("iframe").contentWindow.document;
-              const v = d.querySelector("video");
-              let i = 0;
-              v.dataset.currentIndex = i;
-              v.addEventListener("timeupdate", () => {
-                if (segments[i] && v.currentTime >= segments[i].end) {
-                  i++;
-                  if (i < segments.length) {
+    } else {
+      segments = [{start: 0}, {end: 99999}];
+    }
+    const loop = urlParams.get("loop");
+    const speed = urlParams.get("speed");
+    await new Promise(resolve => {
+      new YT.Player("player", {
+        videoId: ytvid, width: "100%", height: "100%",
+        events: {
+          "onReady": event => {
+            event.target.playVideo();
+            const d = document.querySelector("iframe").contentWindow.document;
+            const v = d.querySelector("video");
+            let i = 0;
+            v.dataset.currentIndex = i;
+            v.addEventListener("timeupdate", () => {
+              if (segments[i] && v.currentTime >= segments[i].end) {
+                i++;
+                if (i < segments.length) {
+                  v.currentTime = segments[i].start;
+                } else {
+                  if (loop) {
+                    i = 0;
                     v.currentTime = segments[i].start;
                   } else {
-                    if (loop) {
-                      i = 0;
-                      v.currentTime = segments[i].start;
-                    } else {
-                      v.pause();
-                    }
+                    v.pause();
                   }
-                  v.dataset.currentIndex = i;
                 }
-              });
-              v.addEventListener("loadedmetadata", () => {
-                if (segments.length > 0) {
-                  v.currentTime = segments[0].start;
-                }
-              });
-            },
-            "onStateChange": event => {
-              if (event.data === YT.PlayerState.ENDED) {
-                resolve();
-              } else if (event.data === YT.PlayerState.PAUSED && Number(document.querySelector("iframe").contentWindow.document.querySelector("video").dataset.currentIndex) == segments.length) {
-                resolve();
+                v.dataset.currentIndex = i;
               }
-            },
-            "onError": event => {
-              console.log("An error occurred:", event.data);
+            });
+            v.addEventListener("loadedmetadata", () => {
+              if (segments.length > 0) {
+                v.currentTime = segments[0].start;
+                if (speed) {
+                  v.playbackRate = speed;
+                }
+              }
+            });
+          },
+          "onStateChange": event => {
+            if (event.data === YT.PlayerState.ENDED) {
+              resolve();
+            } else if (event.data === YT.PlayerState.PAUSED && Number(document.querySelector("iframe").contentWindow.document.querySelector("video").dataset.currentIndex) == segments.length) {
               resolve();
             }
+          },
+          "onError": event => {
+            console.log("An error occurred:", event.data);
+            resolve();
           }
-        });
+        }
       });
-    }
+    });
   }
 };
 function videoSegmentsPlayback(videoURL) {
